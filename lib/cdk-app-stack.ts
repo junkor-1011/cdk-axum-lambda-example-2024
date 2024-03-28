0f7cd98a32ab1f94aba35fbb62c4b86e877a6bc1
junkor-1011 <tomoyukizxy@gmail.com>
2024-03-29 00:36:33 +0900

    refactor: rm unused var

--- lib/cdk-app-stack.ts
+++ lib/cdk-app-stack.ts
@@ -1,13 +1,7 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 import { Stack, type StackProps } from 'aws-cdk-lib';
 import type { Construct } from 'constructs';
 
 import { ApiSystemWithLayer } from './constructs/api-sytem-with-layer';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export class CdkAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
