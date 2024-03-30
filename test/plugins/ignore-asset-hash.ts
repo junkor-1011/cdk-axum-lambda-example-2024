051409aad4e2aa258ad2a406af465dba37bc93a4
junkor-1011 <tomoyukizxy@gmail.com>
2024-03-31 00:34:29 +0900

    test: modify snapshot serializer ignore hash of nested stack template json filename

--- test/plugins/ignore-asset-hash.ts
+++ test/plugins/ignore-asset-hash.ts
@@ -1,6 +1,14 @@
-export const ignoreAssetHashSerializer = {
+import type { SnapshotSerializer } from 'vitest';
+
+export const ignoreHashSerializer = {
   test: (val: unknown) => typeof val === 'string',
   serialize: (val: string) => {
-    return `"${val.replace(/([A-Fa-f0-9]{64}.zip)/, 'HASH-REPLACED.zip')}"`;
+    if (/([A-Fa-f0-9]{64}.zip)/.test(val)) {
+      return `"${val.replace(/([A-Fa-f0-9]{64}.zip)/, 'HASH-REPLACED.zip')}"`;
+    }
+    if (/([A-Fa-f0-9]{64}.json)/.test(val)) {
+      return `"${val.replace(/([A-Fa-f0-9]{64}.json)/, 'HASH-REPLACED.json')}"`;
+    }
+    return `"${val}"`;
   },
-};
+} as const satisfies SnapshotSerializer;
--- test/snapshot.test.ts
+++ test/snapshot.test.ts
@@ -2,14 +2,14 @@ import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { expect, test } from 'vitest';
 import { CdkAppStack } from '../lib/cdk-app-stack';
-import { ignoreAssetHashSerializer } from './plugins/ignore-asset-hash';
+import { ignoreHashSerializer } from './plugins/ignore-asset-hash';
 
 test('snapshot test(root stack)', () => {
   const app = new cdk.App();
   const stack = new CdkAppStack(app, 'TestRootStack');
   const template = Template.fromStack(stack).toJSON();
 
-  expect.addSnapshotSerializer(ignoreAssetHashSerializer);
+  expect.addSnapshotSerializer(ignoreHashSerializer);
   expect(template).toMatchSnapshot();
 
   for (const child of stack.node.children.filter(
