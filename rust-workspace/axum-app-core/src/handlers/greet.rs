3366a07c39370f5f6842af6ac0c588dbe4b507b8
junkor-1011 <tomoyukizxy@gmail.com>
2024-04-14 23:39:52 +0900

    refactor: separate module

---
+++ rust-workspace/axum-app-core/src/handlers/greet.rs
@@ -0,0 +1,42 @@
+use axum::{response::IntoResponse, Json};
+use chrono::Utc;
+use hyper::StatusCode;
+use serde::{Deserialize, Serialize};
+
+pub async fn greet_get() -> impl IntoResponse {
+    tracing::debug!("greet");
+
+    let body = GreetGetResponse {
+        message: "Hello, World!!!".to_string(),
+        date: Utc::now().to_string(),
+    };
+
+    (StatusCode::OK, Json(body))
+}
+
+pub async fn greet_post(Json(payload): Json<GreetPostRequest>) -> impl IntoResponse {
+    let GreetPostRequest { name } = payload;
+    let body = GreetPostResponse {
+        message: format!("Hello, {name}"),
+        date: Utc::now().to_string(),
+    };
+
+    (StatusCode::OK, Json(body))
+}
+
+#[derive(Serialize, Debug)]
+pub struct GreetGetResponse {
+    message: String,
+    date: String,
+}
+
+#[derive(Deserialize)]
+pub struct GreetPostRequest {
+    name: String,
+}
+
+#[derive(Serialize, Debug)]
+pub struct GreetPostResponse {
+    message: String,
+    date: String,
+}
--- rust-workspace/axum-app-core/src/lib.rs
+++ rust-workspace/axum-app-core/src/lib.rs
@@ -1,57 +1,18 @@
+mod handlers;
+
 use axum::{
-    response::IntoResponse,
     routing::{get, post},
-    Json, Router,
+    Router,
 };
-use chrono::Utc;
-use hyper::StatusCode;
-use serde::{Deserialize, Serialize};
+use handlers::greet::{greet_get, greet_post};
 
 pub fn create_app() -> Router {
     Router::new()
         .route("/", get(root))
-        .route("/greet", get(greet))
+        .route("/greet", get(greet_get))
         .route("/greet", post(greet_post))
 }
 
 pub async fn root() -> &'static str {
     "Hello, world!"
-}
-
-pub async fn greet() -> impl IntoResponse {
-    tracing::debug!("greet");
-
-    let body = GreetGetResponse {
-        message: "Hello, World!!!".to_string(),
-        date: Utc::now().to_string(),
-    };
-
-    (StatusCode::OK, Json(body))
-}
-
-pub async fn greet_post(Json(payload): Json<GreetPostRequest>) -> impl IntoResponse {
-    let GreetPostRequest { name } = payload;
-    let body = GreetPostResponse {
-        message: format!("Hello, {name}"),
-        date: Utc::now().to_string(),
-    };
-
-    (StatusCode::OK, Json(body))
-}
-
-#[derive(Serialize, Debug)]
-pub struct GreetGetResponse {
-    message: String,
-    date: String,
-}
-
-#[derive(Deserialize)]
-pub struct GreetPostRequest {
-    name: String,
-}
-
-#[derive(Serialize, Debug)]
-pub struct GreetPostResponse {
-    message: String,
-    date: String,
 }
