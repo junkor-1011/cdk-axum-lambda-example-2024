bc79a690fd89eeb10ca9800e8be275a22d0ba59a
junkor-1011 <tomoyukizxy@gmail.com>
2024-04-15 23:44:39 +0900

    logging

--- rust-workspace/axum-server/src/main.rs
+++ rust-workspace/axum-server/src/main.rs
@@ -1,18 +1,16 @@
-use std::env;
 use std::net::SocketAddr;
 
 use axum_app_core::create_app;
-
-const LOG_LEVEL_ENV: &str = "RUST_LOG";
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
-    let log_level = env::var(LOG_LEVEL_ENV).unwrap_or("info".to_string());
-    env::set_var(LOG_LEVEL_ENV, log_level);
-    // tracing_subscriber::fmt::init();
-    tracing_subscriber::fmt()
-        .json()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "axum_server=info".into()),
+        )
+        .with(tracing_subscriber::fmt::layer().json())
         .init();
 
     let app = create_app();
