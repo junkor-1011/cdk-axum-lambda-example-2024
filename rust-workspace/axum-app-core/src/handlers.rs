1d3adf44471be0dc636e63213b1995d856b89376
junkor-1011 <tomoyukizxy@gmail.com>
2024-04-17 22:58:48 +0900

    add example: get headers & query params

--- rust-workspace/axum-app-core/src/handlers.rs
+++ rust-workspace/axum-app-core/src/handlers.rs
@@ -1,9 +1,28 @@
+use std::collections::HashMap;
+
+use axum::{extract::Query, http::header::HeaderMap};
+
 pub mod greet;
 pub mod reqwest_example;
 
 #[tracing::instrument]
-pub async fn root() -> &'static str {
+pub async fn root(
+    headers: HeaderMap,
+    Query(params): Query<HashMap<String, String>>,
+) -> &'static str {
     tracing::debug!("invoke root");
+
+    for (key, value) in headers {
+        if let Some(key) = key {
+            tracing::debug!("header {key}: {value:#?}");
+        } else {
+            tracing::debug!("header {value:#?}");
+        }
+    }
+
+    for (key, value) in params {
+        tracing::debug!("query param {key}: {value}");
+    }
 
     "Hello, world!"
 }
